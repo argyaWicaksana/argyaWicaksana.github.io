b0c5c2cfcd1d0105d03587a36639513f79e9238b
argyaWicaksana <argya7@gmail.com>
2023-03-11 09:00:06 +0700

    update ui

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+ content: ['./index.html'],
+  theme: {
+    colors: {
+      'black': '#181823',
+      'blue': '#537FE7',
+      'white': '#E9F8F9',
+      'mint': '#C0EEF2',
+      'gray': '#212131'
+    },
+  },
+  plugins: [],
+}
